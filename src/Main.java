6e61871f0ea74640a321806dceb5d2ec33b5ec5a
Rishav0042 <rishavbasnet971@gmail.com>
2025-07-21 12:26:39 +0545

    created

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+    Student student_3= new Student("rishav" ,"rishav@gmail.com","C6");
     }
 }
