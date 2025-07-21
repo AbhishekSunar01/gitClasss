cdccb7e5cba09a0934e05ccb73e67f4a55b48815
Abhishek Sunar <138812561+AbhishekSunar01@users.noreply.github.com>
2025-07-21 12:13:08 +0545

    a

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,34 @@ public class Student {
     private String name;
     private String email;
     private String section;
+
+    public Student(String name, String email, String section) {
+        this.name = name;
+        this.email = email;
+        this.section = section;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
 }
