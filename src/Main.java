2c5c50842c40bca25f6130c93596e35518282c59
Saurav-86 <adhikarisaurav86@gmail.com>
2025-07-21 12:33:53 +0545

    pulled

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
     Student student1=new Student("Saurav","Saurav@gmail.com","C10");
+        System.out.println(student1.getName());
     }
 }
